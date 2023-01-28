6d269046fcbb0492bda9a4dba2d07b1701c9ceb0
W4ltz <Walas.piotr.prywatny@gmail.com>
2023-01-28 13:33:12 +0000

    [WIP] updated setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='BlackBoxFramework',
+      version='0.0.1',
+      description='Generative Testing Framework for zero code applications',
+      author='PetterWaIIace',
+      url='https://github.com/PeterWaIIace/BlackBoxTesting',
+      packages=['src']
+    )
