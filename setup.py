b1f141e79c3ba621e5ac14752b0170cd30f8de5d
W4ltz <40773550+PeterWaIIace@users.noreply.github.com>
2023-01-28 16:25:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(name='BlackBoxFramework',
       version='0.0.1',
       description='Generative Testing Framework for zero code applications',
       author='PetterWaIIace',
-      url='https://github.com/PeterWaIIace/BlackBoxTesting'
+      url='https://github.com/PeterWaIIace/BlackBoxTesting',
+      package_dir={'BlackBox': 'src'}
     )
